9413a7b91ce36dac1a8555062035fe217aed7310
Jason Lines <jlines@solidstatenetworks.com>
2020-07-30 17:02:57 -0700

    add downloadable products customization

--- registration.php
+++ registration.php
@@ -1,1 +1,5 @@
-<?php \Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'SolidStateNetworks_ddpmodule',__DIR__); ?>
+<?php 
+	require_once("Helper/Akamai/TokenAuth.php"); 
+	require_once("Helper/Akamai/InvalidArgumentException.php"); 
+	\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'SolidStateNetworks_ddpmodule',__DIR__); 
+?>
