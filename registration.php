c062e25b927f6bd7bd6e505e3160d2eb8e83276b
Jason Lines <jlines@solidstatenetworks.com>
2020-08-03 19:29:09 -0700

    config lib

--- registration.php
+++ registration.php
@@ -1,3 +1,4 @@
 <?php 
 	\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'SolidStateNetworks_ddpmodule',__DIR__); 
+	require "app/code/SolidStateNetworks/ddpmodule/vendor/autoload.php";
 ?>
