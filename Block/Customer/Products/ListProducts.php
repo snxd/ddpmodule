7ee6435fec72760755a0c43b6714a9e953c18c86
Jason Lines <jlines@solidstatenetworks.com>
2020-08-03 19:55:29 -0700

    config lib

--- Block/Customer/Products/ListProducts.php
+++ Block/Customer/Products/ListProducts.php
@@ -165,6 +165,7 @@ class ListProducts extends \Magento\Framework\View\Element\Template
 
         error_log("even sooner token auth" . getcwd());
         error_log("before token auth");
+        error_log(TokenAuth);
         $edgeAuth = new TokenAuth('aabbccddeeffgg00112233445566', TokenAuth::ALGORITHM_SHA256);
         error_log("after token auth");
         $authUrl = "";//$edgeAuth->generateToken();
