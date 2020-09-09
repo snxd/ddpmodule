ab5e6a778568116b8b03a310ed17e6fae7c8bdd4
Jason Lines <jlines@solidstatenetworks.com>
2020-09-09 15:09:14 -0700

    add admin ui customizations

---
+++ Observer/DDPSave.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SolidStateNetworks\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class DDPSave implements ObserverInterface
+{    
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        //$_product = $observer->getProduct();  // you will get product object
+        //$_sku=$_product->getSku(); // for sku
+
+        $post = $observer->getController();
+        $data = $post->getRequest()->getPost();
+        $productAsArray = $data['product'];
+
+        //$params               = $this->request->getParams();
+        $customFieldData = $data['solid_ddp'];
+
+        error_log("observer .. ");
+        error_log($customFieldData['dlmId']);
+
+    }   
+}
