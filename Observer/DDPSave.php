8e1a0e67610c1dc6dccf61e7fc6966117dcc9166
Jason Lines <jlines@solidstatenetworks.com>
2020-09-09 15:24:25 -0700

    add admin ui customizations

--- Observer/DDPSave.php
+++ Observer/DDPSave.php
@@ -6,16 +6,25 @@ use Magento\Framework\Event\ObserverInterface;
 
 class DDPSave implements ObserverInterface
 {    
+	public function __construct(
+	   Context $context
+	   //other objects
+	) {
+	   $this->context     = $context;
+	   $this->_request   = $context->getRequest();
+	   //other objects
+	}
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         //$_product = $observer->getProduct();  // you will get product object
         //$_sku=$_product->getSku(); // for sku
 
-        $post = $observer->getController();
-        $data = $post->getRequest()->getPost();
-        $productAsArray = $data['product'];
+        //$post = $observer->getController();
+        //$data = $post->getRequest()->getPost();
+        //$productAsArray = $data['product'];
 
-        //$params               = $this->request->getParams();
+        $params               = $this->request->getParams();
         $customFieldData = $data['solid_ddp'];
 
         error_log("observer .. ");
